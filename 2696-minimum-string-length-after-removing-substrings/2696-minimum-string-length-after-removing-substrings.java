08e3cecc1120c5aaa85759787978c830b60f1ccc
Siddhant <86899184+Siddhanttimeline@users.noreply.github.com>
2024-01-28 02:28:42 +0530

    Time: 4 ms (91.72%), Space: 43.5 MB (43.85%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && ((ch == 'B' && stack.peek() == 'A') || (ch == 'D' && stack.peek() == 'C'))) {
+                stack.pop(); 
+            } else {
+                stack.push(ch); 
+            }
+        }
+
+        return stack.size();
+    }
+}
