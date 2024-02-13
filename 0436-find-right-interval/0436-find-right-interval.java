9227c04395af512c08860008f54383c46a5a67b8
Siddhant <86899184+Siddhanttimeline@users.noreply.github.com>
2024-02-14 04:21:09 +0530

    Time: 23 ms (46.13%), Space: 47 MB (84.98%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,36 +1,55 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        int[] result = new int[intervals.length];
 
-        int[] starts = new int[intervals.length];
-        for (int i = 0; i < intervals.length; i++) {
-            starts[i] = intervals[i][0];
+        TreeMap<Integer,Integer> startMap = new TreeMap<>();
+        for(int i=0; i<intervals.length; i++){
+            startMap.put(intervals[i][0],i);
         }
-        Arrays.sort(starts);
 
-        for (int i = 0; i < intervals.length; i++) {
-            int end = intervals[i][1];
-            int getBSIndex = performOps(starts, end, intervals);
-            result[i] = getBSIndex;
+        int[] result = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++){
+            // Integer allows us to hold null values
+            Integer nextStart = startMap.ceilingKey(intervals[i][1]);
+            result[i] = (nextStart != null) ? startMap.get(nextStart) : -1;
         }
 
         return result;
     }
-
-    public int performOps(int[] starts, int target, int[][] intervals) {
-        int index = Arrays.binarySearch(starts, target);
-
-        if (index < 0) {
-            index = -index - 1;
-        }
-
-        if (index < starts.length) {
-            for (int i = 0; i < intervals.length; i++) {
-                if (intervals[i][0] == starts[index]) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
 }
+
+
+
+    // public int[] firstApproachUsingBinarySearch(int[][] intervals) {
+    //     int[] result = new int[intervals.length];
+
+    //     int[] starts = new int[intervals.length];
+    //     for (int i = 0; i < intervals.length; i++) {
+    //         starts[i] = intervals[i][0];
+    //     }
+    //     Arrays.sort(starts);
+
+    //     for (int i = 0; i < intervals.length; i++) {
+    //         int end = intervals[i][1];
+    //         int getBSIndex = performOps(starts, end, intervals);
+    //         result[i] = getBSIndex;
+    //     }
+
+    //     return result;
+    // }
+
+    // public int performOps(int[] starts, int target, int[][] intervals) {
+    //     int index = Arrays.binarySearch(starts, target);
+
+    //     if (index < 0) {
+    //         index = -index - 1;
+    //     }
+
+    //     if (index < starts.length) {
+    //         for (int i = 0; i < intervals.length; i++) {
+    //             if (intervals[i][0] == starts[index]) {
+    //                 return i;
+    //             }
+    //         }
+    //     }
+    //     return -1;
+    // }
