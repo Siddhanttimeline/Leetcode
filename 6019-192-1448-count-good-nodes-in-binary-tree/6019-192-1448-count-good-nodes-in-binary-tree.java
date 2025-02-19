65acfe638a0e9b803dc8375a1591758454aa5cb1
Siddhant <86899184+Siddhanttimeline@users.noreply.github.com>
2025-02-19 10:24:07 +0530

    Time: 1 ms, Memory: 52.6 MB - LeetHub

---
+++ 6019-192-1448-count-good-nodes-in-binary-tree/6019-192-1448-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int goodNodes(TreeNode root) {
+        return count(root, Integer.MIN_VALUE);
+    }
+
+    private int count(TreeNode root, int max) {
+        if (root == null)
+            return 0;
+
+        max = Math.max(max, root.val);
+        int goodNodes = root.val >= max ? 1 : 0;
+
+        return goodNodes + count(root.left, max) + count(root.right, max);
+    }
+}
